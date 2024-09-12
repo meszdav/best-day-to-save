fbff9da7fd604e629c9469bb29ef2a03b0e4468b
Dávid Mészáros <meszarosdavid91@gmail.com>
2024-09-12 21:23:53 +0200

    additional visualizations added

--- src/visualize.py
+++ src/visualize.py
@@ -1,6 +1,7 @@
 import plotly.io as pio
 import plotly.graph_objects as go
 import plotly.express as px
+import polars as pl
 import numpy as np
 
 colors = [
@@ -20,6 +21,13 @@ pio.templates.default = "plotly_dark"
 
 # add colors to the template
 pio.templates[pio.templates.default].layout.update({"colorway": colors})
+
+# add fig.update_traces(marker_line_width=1, marker_line_color="#252525") to layout
+pio.templates[pio.templates.default].layout.update(
+    {
+        "marker": {"line": {"width": 1, "color": "#252525"}},
+    }
+)
 
 
 def plot_total_worth(
@@ -73,6 +81,117 @@ def plot_total_worth(
         line_color="yellow",
         annotation_text=f"Min Total Worth: {np.min(total_worth_all):.2f}",
         annotation_position="top right",
+    )
+
+    return fig
+
+
+def plot_max_diff_distribution(df: pl.DataFrame):
+    if isinstance(df, pl.LazyFrame):
+        df = df.collect()
+
+    plot_df = (
+        df.group_by("period")
+        .agg(
+            pl.min("total_worth").alias("min_total_worth"),
+            pl.max("total_worth").alias("max_total_worth"),
+        )
+        .with_columns(
+            (pl.col("max_total_worth") - pl.col("min_total_worth")).alias("diff")
+        )
+        .with_columns(
+            (pl.col("diff") / pl.col("min_total_worth")).alias("diff_percentage") * 100
+        )
+    )
+
+    values = plot_df["diff_percentage"].to_numpy()
+
+    fig = px.histogram(
+        values,
+        nbins=100,
+        title="Distribution of max total worth difference in % for every possible 15 year period",
+    )
+
+    # change ticks to every second
+    tickvals = list(range(int(values.min()), int(values.max()), 1))
+    fig.update_xaxes(tickmode="array", tickvals=tickvals, showgrid=False)
+    fig.update_yaxes(showgrid=False)
+
+    # q1
+    q1 = np.quantile(values, 0.25)
+    q3 = np.quantile(values, 0.75)
+    median = np.median(values)
+
+    fig.add_vline(
+        x=q1,
+        line_dash="dash",
+        line_color="yellow",
+        annotation_text=f"Q1: {q1:.2f}%",
+        annotation_position="top left",
+    )
+    fig.add_vline(
+        x=median,
+        line_dash="dash",
+        line_color="red",
+        annotation_text=f"Median: {median:.2f}%",
+        annotation_position="right",
+    )
+    fig.add_vline(
+        x=q3,
+        line_dash="dash",
+        line_color="yellow",
+        annotation_text=f"Q3: {q3:.2f}%",
+        annotation_position="top right",
+    )
+
+    fig.update_traces(marker_line_width=1, marker_line_color="#252525")
+
+    fig.update_layout(
+        width=1000,
+        height=500,
+        title="Distribution of max total worth difference in % for every possible 15 year period",
+        xaxis_title="Total Worth Difference in %",
+        yaxis_title="Frequency",
+        template="plotly_dark",
+        showlegend=False,
+    )
+
+    return fig
+
+
+def plot_best_day_distribution(result_df: pl.DataFrame):
+    """Plot the distribution of the best day to invest."""
+    if isinstance(result_df, pl.LazyFrame):
+        result_df = result_df.collect()
+
+    plot_df = result_df.filter(
+        pl.col("total_worth").max().over("period") == pl.col("total_worth")
+    )
+
+    values = (
+        plot_df["day_to_invest"]
+        .value_counts(name="num_occurences")
+        .sort("day_to_invest")
+    )
+    print(values)
+
+    fig = px.bar(
+        x=values["day_to_invest"].to_list(),
+        y=values["num_occurences"].to_list(),
+        title="Distribution of mean total worth for every possible day to invest",
+    )
+
+    fig.update_xaxes(title="Day to Invest")
+    fig.update_yaxes(title="Mean Total Worth")
+
+    fig.update_layout(
+        width=1000,
+        height=500,
+        title="Distribution of mean total worth for every possible day to invest",
+        xaxis_title="Day to Invest",
+        yaxis_title="How many times was the best day to invest",
+        template="plotly_dark",
+        showlegend=False,
     )
 
     return fig
